fb262eb59ac7c701c070445d1a9c9c624b90930b
William Siegrist <wsiegrist@apple.com>
2007-12-08 03:14:52 +0000

    added explicit paths to rm and tclsh in order to run without a PATH.  PortIndex2MySQL.tcl now relies on tclsh being in /opt/local/bin
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base/portmgr/jobs@31804 d073be05-634f-4543-b044-5fe20cf6d1d6

--- jobs/PortIndex2MySQL.tcl
+++ jobs/PortIndex2MySQL.tcl
@@ -1,4 +1,4 @@
-#!/usr/bin/env tclsh
+#!/opt/local/bin/tclsh
 # -*- coding: utf-8; mode: tcl; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- vim:fenc=utf-8:filetype=tcl:et:sw=4:ts=4:sts=4
 #
 # PortIndex2MySQL.tcl
