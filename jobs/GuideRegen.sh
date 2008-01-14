6210b2cf7d4fa02befe053fc7ce2e845935b87c3
William Siegrist <wsiegrist@apple.com>
2008-01-14 03:47:01 +0000

    removing dluke from error emails per his request
    git-svn-id: https://svn.macports.org/repository/macports/trunk/base/portmgr/jobs@32822 d073be05-634f-4543-b044-5fe20cf6d1d6

--- jobs/GuideRegen.sh
+++ jobs/GuideRegen.sh
@@ -13,7 +13,7 @@ LOCKFILE=/tmp/.mp_svn_guide_regen.lock
 # ROOT directory, where everything is. This needs to exist!
 ROOT=/tmp/mpdocs
 # e-mail address to spam in case of failure.
-SPAM_LOVERS=macports-dev@lists.macosforge.org,dluke@geeklair.net,markd@macports.org,wsiegrist@apple.com
+SPAM_LOVERS=macports-dev@lists.macosforge.org,markd@macports.org,wsiegrist@apple.com
 
 # Other settings (probably don't need to be changed).
 SVN_CONFIG_DIR=${ROOT}/svnconfig
