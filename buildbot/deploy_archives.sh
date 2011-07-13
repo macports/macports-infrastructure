303e4c119e17af3703ad33527f4f38b32880451c
Joshua Root <jmr@macports.org>
2011-07-13 15:23:45 +0000

    buildmaster: update for archive deployment
    git-svn-id: https://svn.macports.org/repository/macports/contrib/buildbot@80468 d073be05-634f-4543-b044-5fe20cf6d1d6

--- buildbot/deploy_archives.sh
+++ buildbot/deploy_archives.sh
@@ -13,14 +13,14 @@ if [[ -z "$DLHOST" ]]; then
     DLHOST=""
 fi
 
-# path where it keeps archives
+# path where download server keeps archives
 if [[ -z "$DLPATH" ]]; then
-    DLPATH="/archives"
+    DLPATH="./deployed_archives"
 fi
 
-# path where archives get uploaded to master
+# path where archives get uploaded to buildmaster
 if [[ -z "$ULPATH" ]]; then
-    ULPATH="${PREFIX}/var/macports/software"
+    ULPATH="./archive_staging"
 fi
 
 # private key to use for signing
@@ -29,29 +29,20 @@ if [[ -z "$PRIVKEY" ]]; then
 fi
 
 
-for portname in `cat $PORTLISTFILE`; do
-    if ls logs-*/success/${portname}.log > /dev/null 2>&1 ; then
-        if ./mpexport/base/portmgr/jobs/port_binary_distributable.tcl ${portname}; then
-            echo $portname is distributable
-            portversion=$(${PREFIX}/bin/port info --version --line ${portname})
-            portrevision=$(${PREFIX}/bin/port info --revision --line ${portname})
-            for archive in ${ULPATH}/${portname}/${portname}-${portversion}_${portrevision}[+.]*; do
-                aname=$(basename $archive)
-                echo deploying archive: $aname
-                if [[ -n "$PRIVKEY" ]]; then
-                    openssl dgst -ripemd160 -sign "${PRIVKEY}" -out ./${aname}.rmd160 ${archive}
-                fi
-                if [[ -n "$DLHOST" ]]; then
-                    ssh ${DLHOST} mkdir -p ${DLPATH}/${portname};
-                    rsync -av --ignore-existing ./${aname}.rmd160 ${archive} ${DLHOST}:${DLPATH}/${portname};
-		else
-                    mkdir -p ${DLPATH}/${portname};
-                    rsync -av --ignore-existing ./${aname}.rmd160 ${archive} ${DLPATH}/${portname};
-                fi
-                rm -f ./${aname}.rmd160
-            done
-        else
-            echo $portname is not distributable
-        fi
+for archive in ${ULPATH}/*/*; do
+    portname=$(basename $(dirname $archive))
+    aname=$(basename $archive)
+    echo deploying archive: $aname
+    if [[ -n "$PRIVKEY" ]]; then
+        openssl dgst -ripemd160 -sign "${PRIVKEY}" -out ${ULPATH}/${portname}/${aname}.rmd160 ${archive}
     fi
 done
+
+if [[ -n "$DLHOST" ]]; then
+    rsync -av --ignore-existing ${ULPATH}/ ${DLHOST}:${DLPATH}
+else
+    rsync -av --ignore-existing ${ULPATH}/ ${DLPATH}
+fi
+
+# clean up after ourselves
+rm -rf $ULPATH
